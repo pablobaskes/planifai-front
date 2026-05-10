d5bfab6115f95f32c8d0eb9992388cbd36471114
Pablo <pablovazquezbarrena97@gmail.com>
2026-05-11 01:12:23 +0200

    feat: add completation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'inventory',
+    loadComponent: () =>
+      import('./features/inventory/pages/inventory-page/inventory-page').then(m => m.InventoryPage)
+  },
+  {
     path: 'diet',
     loadChildren: () =>
       import('./features/diet/diet.routes').then(m => m.DIET_ROUTES)
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
@@ -20,5 +22,6 @@ describe('App', () => {
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('router-outlet')).toBeTruthy();
+    expect(compiled.querySelector('a[routerLink="/inventory"]')).toBeTruthy();
   });
 });
