1a322979e85226521072f8d7f8f0d6b92aae3f3d
Pablo <pablovazquezbarrena97@gmail.com>
2026-05-16 21:12:32 +0200

    chore: add docker file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8083/api/v1'
-  // Cuando integres con gateway: 'http://localhost:8080/diet-service/api/v1'
+  apiUrl: 'http://localhost:8080/api/v1'
 };
